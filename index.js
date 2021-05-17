2216b4ad340f8dc098621d64663de5ed31462135
Altaf12sees <altaf12sees@gmail.com>
2021-05-17 10:01:11 +0400

    blank page

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-var _ = require('underscore');
-var result = _.contains([1,2,3], 2);
-console.log(result);
-const http =  require('http');
+// var _ = require('underscore');
+// var result = _.contains([1,2,3], 2);
+// console.log(result);
+// const http =  require('http');
 
-const server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.write("Hello World");
-        res.end();
-    }
-});
-// server.on('connection', (socket)=>{
-//     console.log('New Connection');
-// })
-server.listen(3000);
-console.log('Listening on port 3000...');
-// const EventEmitter = require('events');
-// const emitter = new EventEmitter()
+// const server = http.createServer((req, res)=>{
+//     if(req.url === '/'){
+//         res.write("Hello World");
+//         res.end();
+//     }
+// });
+// // server.on('connection', (socket)=>{
+// //     console.log('New Connection');
+// // })
+// server.listen(3000);
+// console.log('Listening on port 3000...');
+// // const EventEmitter = require('events');
+// // const emitter = new EventEmitter()
 
-// emitter.on('messageLogged', (arg) => {
-//     console.log('Listner..', arg)
-// })
-// emitter.emit('messageLogged', {id: 1, url: 'http://'});
+// // emitter.on('messageLogged', (arg) => {
+// //     console.log('Listner..', arg)
+// // })
+// // emitter.emit('messageLogged', {id: 1, url: 'http://'});
 
 
-// const fs = require('fs');
+// // const fs = require('fs');
 
-// const files = fs.readdirSync('./');
-// console.log(files);
-// fs.readdir('./', function(err, files){
-//     if(err) console.log(err);
-//     else console.log(files);
-// })
-// function sayHello(){
-//     console.log("HI");
-// }
-// sayHello();
+// // const files = fs.readdirSync('./');
+// // console.log(files);
+// // fs.readdir('./', function(err, files){
+// //     if(err) console.log(err);
+// //     else console.log(files);
+// // })
+// // function sayHello(){
+// //     console.log("HI");
+// // }
+// // sayHello();
